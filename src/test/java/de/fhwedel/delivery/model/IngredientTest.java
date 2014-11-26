7fa5a176facc2b81ca62ecdc240147999b39c881
Tim Pauls <tim.pauls.hh@gmail.com>
2014-11-27 00:19:29 +0100

    Added test for Ingredient and Pizza and assertions to previous test.

---
+++ src/test/java/de/fhwedel/delivery/model/IngredientTest.java
@@ -0,0 +1,36 @@
+package de.fhwedel.delivery.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IngredientTest {
+
+    private Ingredient zucker;
+
+    @Before
+    public void setUp() throws Exception {
+        zucker = new Ingredient(0l, "Zucker", new BigDecimal("1337"));
+    }
+
+    @Test
+    public void equality() throws Exception {
+        Ingredient equal = new Ingredient(0l, "Zucker", new BigDecimal("1337"));
+
+        assertThat(zucker).isEqualTo(equal);
+    }
+
+    @Test
+    public void notEquality() throws Exception {
+        Ingredient diffId = new Ingredient(1l, "Zucker", new BigDecimal("1337"));
+        Ingredient diffName = new Ingredient(0l, "Salz", new BigDecimal("1337"));
+        Ingredient diffCost = new Ingredient(1l, "Zucker", new BigDecimal("42"));
+
+        assertThat(zucker).isNotEqualTo(diffId);
+        assertThat(zucker).isNotEqualTo(diffName);
+        assertThat(zucker).isNotEqualTo(diffCost);
+    }
+}
---
+++ src/test/java/de/fhwedel/delivery/model/PizzaTest.java
@@ -0,0 +1,45 @@
+package de.fhwedel.delivery.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PizzaTest {
+
+    private Pizza empty;
+    private Pizza salamiCheese;
+
+    @Before
+    public void setUp() throws Exception {
+        empty = Pizza.empty();
+        empty.setId(0l);
+
+        salamiCheese = Pizza.empty().addIngredient(Ingredient.SALAMI).addIngredient(Ingredient.CHEESE);
+        salamiCheese.setId(1l);
+    }
+
+    @Test
+    public void equality() throws Exception {
+        Pizza equalsEmpty = Pizza.empty();
+        equalsEmpty.setId(0l);
+
+        Pizza equalsSalamiCheese = Pizza.empty().addIngredient(Ingredient.SALAMI).addIngredient(Ingredient.CHEESE);
+        equalsSalamiCheese.setId(1l);
+
+        assertThat(empty).isEqualTo(equalsEmpty);
+        assertThat(salamiCheese).isEqualTo(equalsSalamiCheese);
+    }
+
+    @Test
+    public void notEquality() throws Exception {
+        Pizza notEqualsEmpty = Pizza.empty();
+        notEqualsEmpty.setId(1l);
+
+        Pizza notEqualsSalamiCheese = Pizza.empty().addIngredient(Ingredient.TOMATO_SAUCE).addIngredient(Ingredient.CHEESE);
+        notEqualsSalamiCheese.setId(1l);
+
+        assertThat(empty).isNotEqualTo(notEqualsEmpty);
+        assertThat(salamiCheese).isNotEqualTo(notEqualsSalamiCheese);
+    }
+}
